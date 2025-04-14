e9a0c0d1bb54b9469d66899bda2cb4d5d26de484
Artem <luostra@yandex.ru>
2025-04-14 20:22:58 +0300

    обновлён App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,65 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Button, Menubar } from 'primevue'
+import { ref } from 'vue'
+//    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+const items = ref([
+  {
+    label: 'Задачи',
+    icon: 'pi pi-pen-to-square',
+    path: '/',
+  },
+  {
+    label: 'О программе',
+    icon: 'pi pi-info',
+    path: '/about',
+  },
+  {
+    label: 'Войти',
+    icon: 'pi pi-user',
+    path: '/login',
+  },
+])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <Menubar
+    :model="items"
+    class="!fixed !backdrop-blur-sm !bg-emerald-500/40 w-full h-auto z-10000"
+    :pt="{
+      root: { class: '!flex !justify-between !gap-4' },
+      rootList: { class: 'order-2 !ml-auto' },
+      itemContent: { class: '' },
+      button: {
+        class: 'order-2 !ml-auto !text-white hover:!bg-emerald-600 active:!bg-emerald-700',
+      },
+    }"
+  >
+    <template #start>
+      <img
+        width="115"
+        height="35"
+        viewBox="0 0 115 35"
+        fill="none"
+        src="./assets/logoFull.png"
+        class=""
+      />
+    </template>
+    <template #item="{ item, props }">
+      <a class="flex items-center" v-bind="props.action">
+        <RouterLink :to="item.path">
+          <span :class="item.icon" />
+          <span class="pl-1">{{ item.label }}</span>
+        </RouterLink>
+      </a>
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2"></div>
+    </template>
+  </Menubar>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style scoped></style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
